cb420decb201835245efdfe097ca55dfb57f1fdc
hesham2022 <47603326+hesham2022@users.noreply.github.com>
2023-11-16 14:04:41 +0200

    push

--- android/generate_keystore.sh
+++ android/generate_keystore.sh
@@ -1,1 +1,1 @@
-keytool -genkey -v -keystore zezo.jks -alias zezo -storepass flutter -keypass flutter -keyalg RSA -keysize 2048 -validity 10000
+keytool -genkey -v -keystore zezo.jks -alias zezo -storepass flutter -keypass flutter -keyalg RSA -keysize 2048 -validity 10000 -storetype JKS
